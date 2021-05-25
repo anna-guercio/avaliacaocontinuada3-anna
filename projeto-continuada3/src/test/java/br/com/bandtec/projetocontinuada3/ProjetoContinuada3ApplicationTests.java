a00d8adc8b77314d9fde25dc3364c70714197b61
anna-guercio <anna.guercio@bandtec.com.br>
2021-05-25 01:49:27 -0300

    Criando o projeto

---
+++ projeto-continuada3/src/test/java/br/com/bandtec/projetocontinuada3/ProjetoContinuada3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.projetocontinuada3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoContinuada3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
